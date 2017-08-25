50a1b666521bd233518d33e5ec7d75d0c2917723
Oleksandr Kradenkov <kradenkov@gmail.com>
2017-08-25 09:38:47 +0300

    introduced UIViewController+RootView.swift

---
+++ GravyKit/GravyKit/UIViewController+RootView.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+RootView.swift
+//  GravyKit
+//
+//  Created by Alexander Kradenkov on 8/25/17.
+//
+//
+
+import UIKit
+
+public protocol RootViewGettable {
+	associatedtype ViewType
+	var rootView: ViewType? { get }
+}
+
+extension RootViewGettable where Self: UIViewController {
+	public var rootView: ViewType? {
+		return self.loadedView as? ViewType
+	}
+	
+	/// Returns the view controller's view if loaded, nil if not.
+	private var loadedView: UIView? {
+		if #available(iOS 9.0, *) {
+			return self.viewIfLoaded
+		} else {
+			return self.isViewLoaded ? view : nil
+		}
+	}
+}
---
+++ GravyKit/GravyKitTests/RootViewGettableTests.swift
@@ -0,0 +1,69 @@
+//
+//  RootViewGettableTests.swift
+//  GravyKit
+//
+//  Created by Oleksandr Kradenkov on 8/25/17.
+//
+//
+
+import XCTest
+import GravyKit
+
+class RootViewGettableTests: XCTestCase {
+	var controller: RootViewGettableController!
+	
+	override func setUp() {
+		super.setUp()
+		
+		self.controller = RootViewGettableController.init()
+	}
+	
+	
+	func testRootViewMustBeNilBeforeViewIsAccessed() {
+		guard let controller = self.controller else {
+			XCTFail("controller must not be nil")
+			
+			return
+		}
+		
+		XCTAssertFalse(controller.isViewLoaded)
+		XCTAssertNil(controller.rootView)
+	}
+	
+	func testRootViewMustNotBeNilAfterViewIsAccessed() {
+		guard let controller = self.controller else {
+			XCTFail("controller must not be nil")
+			
+			return
+		}
+		
+		XCTAssertFalse(controller.isViewLoaded)
+		
+		controller.loadViewIfNeeded()
+		
+		XCTAssertTrue(controller.isViewLoaded)
+		XCTAssertNotNil(controller.view)
+		
+		XCTAssertTrue(controller.isViewLoaded)
+		XCTAssertNotNil(controller.rootView)
+		XCTAssertNotNil(controller.view)
+		
+		_ = controller.rootView.map {
+			XCTAssertTrue($0.isKind(of: CustomView.self),
+			              "rootView must be inherited from CustomView")
+		}
+		
+		XCTAssertEqual(controller.view, self.controller.rootView)
+	}
+}
+
+class CustomView: UIView { }
+
+class RootViewGettableController: UIViewController, RootViewGettable {
+	typealias ViewType = CustomView
+	
+	override func loadView() {
+		super.loadView()
+		self.view = CustomView()
+	}
+}
